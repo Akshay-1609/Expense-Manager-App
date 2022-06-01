c007d845c7cadedc40765e28913819093bdb8ebf
unknown <akshaymakwana9577@gmail.com>
2022-06-02 00:36:16 +0530

    project created

---
+++ lib/constant/color_constsnt.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class colorConstant {
+
+  static Color white =const Color(0xffffffff);
+  static Color black = const Color(0xff000000);
+  static Color darkGreen = const Color(0xff1A3C40);
+  static Color lightGrey = const Color(0xff8C9D9F);
+}
